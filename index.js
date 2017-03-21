73e69d6e53b5da571a9887d3f374f6f24541d1a9
Marcel Toele <EleotleCram@gmail.com>
2017-03-21 16:30:57 +0100

    lib/log-util.js: Added rudimentary conditional debug logging

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const bus = require('./lib/bus');
 const {RootAction} = require('./lib/actions');
 const spec = require('./lib/spec');
+const {log} = require('./lib/log-util');
 
 module.exports = {
 	bus,
 	RootAction,
 	spec,
+	log,
 };
